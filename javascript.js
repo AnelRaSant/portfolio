94cc2fe60b23a40812797533c1fe1cdcecad087f
AnelRaSant <anel.rmrz.stgo7@gmail.com>
2020-06-08 23:05:57 -0500

    MADE ALL PAGES MOBILE RESPONSIVE + ADDED A DATE DEFINER ON ABOUT ME PAGE

--- javascript.js
+++ javascript.js
@@ -1,3 +1,38 @@
+function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  hourUpdater();
+
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(hourUpdater, 15000);
+
+
+   // display current day on page
+   $("#currentDay").text(moment().format("dddd, MMMM Do"));
+});
+
+
 
 //UNIVERSE BACKGROUND
     function createStars(type, quantity) {
